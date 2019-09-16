0ebd939a27169ee8f009a50e0d2e75025e6e1c2d
Andy Ryan <awryan88@gmail.com>
2019-09-15 20:34:37 -0400

    adding project

---
+++ build/precache-manifest.1f74e8fd605bbf37bcb5d068529608f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42d11058f7e61f41e1c6500241937461",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "6e37525aa59c31448187",
+    "url": "/static/css/main.81a99535.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "6e37525aa59c31448187",
+    "url": "/static/js/main.a1a404ad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
